77d2c08f32381bdc6a469c318de885b6ebb6c879
pdasf <133340224+pdasf@users.noreply.github.com>
2023-06-07 15:02:05 +0800

    Update common.go

--- shardkv/common.go
+++ shardkv/common.go
@@ -1,5 +1,7 @@
 package shardkv
 
+import "mit6824/labgob"
+
 //
 // Sharded key/value server.
 // Lots of replica groups, each running Raft.
@@ -14,31 +16,102 @@ const (
 	ErrNoKey       = "ErrNoKey"
 	ErrWrongGroup  = "ErrWrongGroup"
 	ErrWrongLeader = "ErrWrongLeader"
+	ErrTimeOut     = "ErrTimeOut"
+	ErrServer      = "ErrServer"
 )
 
 type Err string
 
-// Put or Append
+func init() {
+	labgob.Register(CleanShardDataArgs{})
+	labgob.Register(MergeShardData{})
+}
+
 type PutAppendArgs struct {
-	// You'll have to add definitions here.
-	Key   string
-	Value string
-	Op    string // "Put" or "Append"
-	// You'll have to add definitions here.
-	// Field names must start with capital letters,
-	// otherwise RPC will break.
+	Key       string
+	Value     string
+	Op        string
+	ClientId  int64
+	CommandId int64
+	ConfigNum int
 }
 
 type PutAppendReply struct {
 	Err Err
 }
 
+func (c *PutAppendArgs) copy() PutAppendArgs {
+	r := PutAppendArgs{
+		Key:       c.Key,
+		Value:     c.Value,
+		Op:        c.Op,
+		ClientId:  c.ClientId,
+		CommandId: c.CommandId,
+		ConfigNum: c.ConfigNum,
+	}
+	return r
+}
+
 type GetArgs struct {
-	Key string
-	// You'll have to add definitions here.
+	Key       string
+	ClientId  int64
+	CommandId int64
+	ConfigNum int
 }
 
 type GetReply struct {
 	Err   Err
 	Value string
+}
+
+func (c *GetArgs) copy() GetArgs {
+	r := GetArgs{
+		Key:       c.Key,
+		ClientId:  c.ClientId,
+		CommandId: c.CommandId,
+		ConfigNum: c.ConfigNum,
+	}
+	return r
+}
+
+type FetchShardDataArgs struct {
+	ConfigNum int
+	ShardNum  int
+}
+
+type FetchShardDataReply struct {
+	Success        bool
+	CommandIndexes map[int64]int64
+	Data           map[string]string
+}
+
+func (reply *FetchShardDataReply) Copy() FetchShardDataReply {
+	res := FetchShardDataReply{
+		Success:        reply.Success,
+		Data:           make(map[string]string),
+		CommandIndexes: make(map[int64]int64),
+	}
+	for k, v := range reply.Data {
+		res.Data[k] = v
+	}
+	for k, v := range reply.CommandIndexes {
+		res.CommandIndexes[k] = v
+	}
+	return res
+}
+
+type CleanShardDataArgs struct {
+	ConfigNum int
+	ShardNum  int
+}
+
+type CleanShardDataReply struct {
+	Success bool
+}
+
+type MergeShardData struct {
+	ConfigNum      int
+	ShardNum       int
+	CommandIndexes map[int64]int64
+	Data           map[string]string
 }
